caa890b720cec16e594adc35b821df216bff1dcd
Salil Ponde <salil.ponde@gmail.com>
2023-10-19 15:41:56 +0530

    Add Project button fix

--- frontend/app/(orglevel)/projects/page.tsx
+++ frontend/app/(orglevel)/projects/page.tsx
@@ -4,32 +4,22 @@ import { cn } from "@/lib/utils"
 import React from "react"
 import ProjectCard from "./project-card"
 
-import Link from "next/link"
+import AddProjectDialog from "@/components/app-project-dialog"
 import MainContainer from "@/components/main-container"
-import { Button } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
 import useProjects from "@/hooks/useProjects"
 
 type Props = {}
 
 const ProjectsPage = (props: Props) => {
   const { projects, isError } = useProjects()
- 
-  if(isError){
-    return (<p>Something whent wrong try again!</p>
-      )
+
+  if (isError) {
+    return <p>Something whent wrong try again!</p>
   }
 
   return (
     <MainContainer props={{ className: "" }}>
-      <div className={cn("")}>
-        <Link href="/targets">
-          <Button>
-            <Icons.plus className="mr-2 h-4 w-4" />
-            Add Project
-          </Button>
-        </Link>
-      </div>
+      <AddProjectDialog />
       <div className={cn("flex flex-wrap gap-4")}>
         {projects?.items &&
           projects.items.map((project) => (
