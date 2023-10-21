b0ee7e1956bfd306721e408daf86f79c5c20e1f0
Salil Ponde <salil.ponde@gmail.com>
2023-10-21 22:28:09 +0530

    Fixed nav item highlighting logic

--- frontend/components/nav/NavProjectLevel.tsx
+++ frontend/components/nav/NavProjectLevel.tsx
@@ -35,7 +35,7 @@ export default function NavProjectLevel({
             <Link
               key={i}
               className={
-                currentRoute.includes(navItem.href)
+                currentRoute.match(new RegExp(`^/[\\d]+${navItem.href}`))
                   ? "bg-nav rounded-md bg-blue-500 px-3 py-2 text-sm font-medium text-white"
                   : "rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-blue-500 hover:bg-opacity-75"
               }
